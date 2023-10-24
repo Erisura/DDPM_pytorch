0592ac71a90e21770427403f9ffd9ca48392cd2c
erisura <erisura@outlook.com>
2023-10-24 15:30:22 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import torch
 from torchvision.utils import save_image
 from diffusion import *
+from dataset import *
+import matplotlib.pyplot as plt
 
-model = torch.load('./trained_models/ddpm.pth')
-imgs = sample(model, 28,28, batch_size=4, channels=1)
-imgs = (imgs + 1)/2
-save_image(imgs)
-save_image(imgs, str('result_imgs' + f'samples.png'),nrow=2)
+model = torch.load('./trained_models/ddpm.pth',map_location='cpu')
+# sample 64 images
+samples = sample(model, img_size=img_size, batch_size=64, channels=channels)
 
+# show a random one
+random_index = 5
+plt.imshow(samples[-1][random_index].reshape(img_size, img_size, channels), cmap="gray")
